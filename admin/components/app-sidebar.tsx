4c9e62288dd3be15d43b8e72b47dab10234b7ba7
Aliyan Sajid <aliyansajid127@gmail.com>
2025-06-15 11:37:09 +0500

    feat(admin/layout): implement sidebar and header components

---
+++ admin/components/app-sidebar.tsx
@@ -0,0 +1,92 @@
+"use client";
+
+import * as React from "react";
+import {
+  AudioWaveform,
+  Battery,
+  BookOpen,
+  Bot,
+  Caravan,
+  Command,
+  Droplets,
+  GalleryVerticalEnd,
+  Settings2,
+  SquareTerminal,
+  Users,
+} from "lucide-react";
+
+import { NavMain } from "@/components/nav-main";
+import { NavUser } from "@/components/nav-user";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarRail,
+} from "@/components/ui/sidebar";
+
+const data = {
+  user: {
+    name: "aliyan",
+    email: "aliyan@me.com",
+    avatar: "/avatars/shadcn.jpg",
+  },
+
+  navMain: [
+    {
+      title: "Users",
+      url: "/users",
+      icon: Users,
+      isActive: true,
+    },
+    {
+      title: "Trailers",
+      url: "/trailers",
+      icon: Caravan,
+    },
+    {
+      title: "Mopeka",
+      url: "/mopeka",
+      icon: Droplets,
+    },
+    {
+      title: "BMS",
+      url: "/bms",
+      icon: Battery,
+    },
+  ],
+};
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible="icon" {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton size="lg" asChild>
+              <a href="#">
+                <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
+                  <Command className="size-4" />
+                </div>
+                <div className="grid flex-1 text-left text-sm leading-tight">
+                  <span className="truncate font-medium">Rotorsync</span>
+                  <span className="truncate text-xs">Enterprise</span>
+                </div>
+              </a>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={data.navMain} />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
+      <SidebarRail />
+    </Sidebar>
+  );
+}
